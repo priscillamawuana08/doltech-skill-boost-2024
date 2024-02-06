97cd18c6a7c75ab5b8e70aba6b666485a8ac9426
priscillamawuana08 <priscillabonsuktu@gmail.com>
2024-02-06 10:13:02 +0000

    assignment

--- First-Assignment/ProgrammingChallenge/StarPattern.java
+++ First-Assignment/ProgrammingChallenge/StarPattern.java
@@ -6,6 +6,7 @@ public class StarPattern {
         int n = 4;
 
         // Upper part of the pattern
+        //
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n - i; j++) {
                 System.out.print(" ");
